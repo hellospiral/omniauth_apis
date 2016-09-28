d101baa589508294009be6c3559d3bb0c2b7a11f
Matt Carlson and Noah Yasskin <matt+noah@>
2016-09-28 10:16:06 -0700

    allow users to search songs on spotify

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.5'
 gem "animate-rails"
 gem "chartkick"
 gem 'devise'
+gem 'rspotify'
 
 group :development, :test do
   gem 'byebug', platform: :mri
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,12 @@
 class SongsController < ApplicationController
   def index
+    if !params[:song_name].empty?
+      @songs = RSpotify::Track.search(params[:song_name])
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
   end
 end
